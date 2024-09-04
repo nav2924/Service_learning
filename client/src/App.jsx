039bfd23b0cd45e897fcb7e480b8763c6ee7b2b0
nav2924 <navi29012004@gmail.com>
2024-09-04 20:27:59 +0530

    Build error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,9 +9,9 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/approach" element={<Approach />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/approach" element={<Approach />} />
         </Routes>
       </Router>
     </SmoothScreen>
