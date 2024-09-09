d90630886d6d71a747b2a19bbb5c4477b9bb4861
nav2924 <navi29012004@gmail.com>
2024-09-09 09:41:11 +0530

    Rollback

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home, About, Contact, Approach } from "./pages/index";
 import { SmoothScreen } from "./components/index";
 
 function App() {
   return (
     <SmoothScreen>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/approach" element={<Approach />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/approach" element={<Approach />} />
+        </Routes>
+      </Router>
     </SmoothScreen>
   );
 }
