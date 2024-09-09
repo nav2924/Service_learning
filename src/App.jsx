09fc6476175254ab1127e297d1928789c7315fe7
nav2924 <navi29012004@gmail.com>
2024-09-09 09:38:13 +0530

    check

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Home, About, Contact, Approach } from "./pages/index";
 import { SmoothScreen } from "./components/index";
 
 function App() {
   return (
     <SmoothScreen>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/contact" element={<Contact />} />
-          <Route exact path="/approach" element={<Approach />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/approach" element={<Approach />} />
+      </Routes>
     </SmoothScreen>
   );
 }
